83bd19934d99bcb222caacc0a77f58cfcafcd394
Lexi Tran <hazukitran@gmail.com>
2016-04-25 22:26:43 +0700

    Setting up Sinatra

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+get '/' do
+  "hello world!"
+end
+
+get '/secret' do
+  "What is your favourite band? Mine is Lexi's Midnight Runners!"
+end
+
+get '/monkey_tennis' do
+  "It was like bigger than the world cup, according to Adam."
+end
