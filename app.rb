b29d656cb067e419292f2b23b92eca014aa0a2ba
Lexi Tran <hazukitran@gmail.com>
2016-04-25 23:01:41 +0700

    Returning HTML

--- app.rb
+++ app.rb
@@ -11,3 +11,9 @@ end
 get '/monkey_tennis' do
   "It was like bigger than the world cup, according to Adam."
 end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
