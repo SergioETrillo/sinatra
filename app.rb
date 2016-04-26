2f7dc88e2ff137ca08ec9351181661ab57e28935
AdamSkuse <a.t.skuse@gmail.com>
2016-04-26 15:34:12 +0100

    Step 13 done

--- app.rb
+++ app.rb
@@ -5,21 +5,21 @@ get '/' do
 end
 
 #this one is probably more advanced...
-get '/cat-form' do
-  erb :cat_form
+get '/cat_form' do
+  erb (:cat_form)
 end
 
 get '/monkey_tennis' do
   "It was like bigger than the world cup, according to Adam."
 end
 
-get '/random-cat' do
+get '/random_cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
 
-get '/named-cat' do
-  p params[:name]
+get '/named_cat' do
+  p params
   @name = params[:name]
-  erb(:index)
+  erb(:named_cat)
 end
